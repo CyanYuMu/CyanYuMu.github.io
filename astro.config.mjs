9fbb6a02e54443825a90c350d515ef1ccfecbede
cyanyumu <CyanYuMu@Outlook.com>
2025-12-11 15:55:24 +0800

    这次忽略了前缀再试试

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,6 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://CyanYuMu.github.io",
-	base: "my-blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
